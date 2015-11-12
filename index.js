3e8241ec82b6de7f6b1c49235b593d5ea8b80464
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-11-12 17:14:46 -0500

    WIP 1/2 way thru cleanup

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-/* jshint esnext: true */
 const Promise = require('bluebird');
 const request = Promise.promisifyAll(require('superagent'));
 const resolve = require('url').resolve;
 const join = require('path').join;
+require("babel-core/register");
+const sheets = require('./lib/sheets');
 
 //@see https://github.com/npm/download-counts
 const npmUriBase = 'https://api.npmjs.org/downloads/';
@@ -24,48 +25,15 @@ const getDownloadCounts = pkgs => {
     .map(getBody);
 };
 
-//turn downloadCount results into a row for gdocs
-const buildRow = pkgstats => {
-  return pkgstats.reduce((out, pkgstats) => {
-      out.start = pkgstats.start;   //redundant
-      out.end = pkgstats.end;       //redundant
-      out[pkgstats.package] = pkgstats.downloads;
-      return out;
-    }, {});
-};
-
 //@see https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method
-const credsPath = join(__dirname, '../../google-creds.json');              //CHANGE THIS AS NEEDED
-
+const credsPath = join(__dirname, '../google-creds.json');              //CHANGE THIS AS NEEDED
 const creds = require(credsPath);
 const sheet_id = '1cRwJdc89Tubpzobm0yJQF4qXwIHs6AwER3uJYor8Di4';
-const GoogleSheets = require('google-spreadsheet');
-//this ends up getting used "globally" here :(
-const doc = Promise.promisifyAll(new GoogleSheets(sheet_id));
+const doc = sheets(sheet_id, creds);
 
-//sheet-collection util fns
-const getinfo = () => doc.getInfoAsync();
-const logDocTitle = doc_info => log(doc_info.title + ' is loaded');
-const getSheet = (idx) => (allSheets) => Promise.promisifyAll(allSheets.worksheets[idx]);
-
-//single-sheet fns
-//@TODO promisify the individual rows (this sheets thing has a bit of a crazy API tbh :/)
-const getRows = sheet => sheet.getRowsAsync();
-
-//rows fns
-const logRowInfo = (rows) => log('rows:', rows.length);
-//shifting index here to match normal worksheet 
-const addRow = (sheet, rowData) => sheet.addRowAsync(rowData);
-
-const getFrameworksSheet = () => {
-  return doc
-    .useServiceAccountAuthAsync(creds)
-    .then(getinfo)
-    .tap(logDocTitle)
-    .then(getSheet(0));
-};
-
+//getDownloadCounts(pkgs).then(log);
 Promise.join(
-  getFrameworksSheet(),                   //get google sheet
-  getDownloadCounts(pkgs).then(buildRow), //get new NPM data & build row
-  addRow);                                //send it to google
+  doc.getSheet(0),                               //get google sheet
+  getDownloadCounts(pkgs).tap(log).then(doc.buildNpmRow), //get new NPM data & build row
+  doc.addRow                                     //send it to google
+).catch(err);
